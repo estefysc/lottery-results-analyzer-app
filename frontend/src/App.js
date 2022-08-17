9922b63663b90193e0154dd107de97e78f7eccf4
Estefania Sanchez <estefysc@gmail.com>
2022-08-17 18:25:45 -0400

    Added Loading Pag'e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import NumberList from './components/NumberList';
 import Particle from "./components/Particle";
 import Navigation from "./components/Navigation"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoadingScreen from "./components/LoadingScreen";
 
 
 function App() {
-  return (
-      <div>
-          <Navigation fixed="top"/>
-          <Particle />
-          <div>
-              <NumberList />
-          </div>
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    });
 
-      </div>
-  );
+    return (
+        <div>
+            {loading ?
+                (<LoadingScreen />) :
+                (<div>
+                    <Navigation fixed="top"/>
+                    <Particle />
+                    <div>
+                        <NumberList />
+                    </div>
+                </div>)
+            }
+        </div>
+    );
 }
 
 export default App;
