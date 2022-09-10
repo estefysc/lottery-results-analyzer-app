84c66eb6ac88f28ddc66af46b3aefa9a625339f0
Estefania Sanchez <estefysc@gmail.com>
2022-09-10 16:19:28 -0400

    Displayed even/odd percentages

--- frontend/src/components/EvenOdds.js
+++ frontend/src/components/EvenOdds.js
@@ -4,23 +4,53 @@ import axios from "axios";
 import "./EvenOdds.css";
 
 function EvenOdds() {
-    const [text, setText] = useState("");
+    const [data, setData] = useState("");
 
-    // function getNumData() {
-    //     axios.get("http://localhost:5000/frequentNums", {crossdomain: true})
-    //         .then(response => {
-    //             setText(response.data.freqNums.map(
-    //                 number => number.toString()
-    //             ).join(", "));
-    //         });
-    // }
+    function getData() {
+        axios.get("http://localhost:5000/evenOdd", {crossdomain: true})
+            .then(response => {
+                setData(response.data);
+            });
+    }
 
-    // useEffect(() => getNumbers(), []);
+    useEffect(() => getData(), []);
 
-    return(
-
+    return (
         <div>
-            <p className='number-text'>{text}</p>
+            <h1 className="first-title">Even and Odd Numbers</h1>
+            <p className="paragraph">
+                {/*Total even numbers: {data.numbersData.totalEvenNumbers}*/}
+                {/*Total odd numbers: {data.numbersData.totalOddNumbers}*/}
+            </p>
+
+            <h1 className="second-title">Even/Odd Numbers Info</h1>
+            <p className="paragraph">
+                Average even in one set: {Math.round(data.averageEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Average odd in one set: {Math.round(data.averageOdd * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent zero even: {Math.round(data.percentZeroEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent one even: {Math.round(data.percentOneEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent two even: {Math.round(data.percentTwoEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent three even: {Math.round(data.percentThreeEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent four even: {Math.round(data.percentFourEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent five even: {Math.round(data.percentFiveEven * 100) / 100}
+            </p>
+            <p className="paragraph">
+                Percent six even: {Math.round(data.percentSixEven * 100) / 100}
+            </p>
         </div>
     )
 }
